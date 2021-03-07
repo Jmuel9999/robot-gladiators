59056eeef40ac7a190ed868fc3fec3c69837e0ba
jasonmueller <jmuel9999@gmail.com>
2021-03-07 14:42:54 -0600

    Added more JS for the beginning window alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
+var playerName = window.prompt("What is your robot's name?");//"what's the player name?" is the ANSWER for the variable
+
+console.log(playerName);//allows to test our code without interrupting page with an alert
+
+window.alert(playerName);//enters robot's name after it is declared in another pop up window
+//Think of alerts as getting information to the USER, not to the developer
+
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+//fight(); // this is how to call a function, at the bottom of the js
+
