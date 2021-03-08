a82e1c5b2b81711562dbcd096a50bf05ae600794
jasonmueller <jmuel9999@gmail.com>
2021-03-07 22:55:44 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,25 @@ var playerMoney = 10;
 
 //console.log(enemyName = "Roborto", enemyHealth = 50, enemyAttack = 12);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];//this is an array, in case we need 100s of other robots
 var enemyHealth = 50;
 var enemyAttack = 12;
+//console.log(enemyNames);// array argument
+//console.log(enemyNames[0]);
+//console.log(enemyNames[1]);
+//console.log(enemyNames[2]);
+//console.log(enemyNames.length);
+var fight = function(enemyName) {
+for(var i = 0; i < enemyNames.length; i++) {//var i = 0; this is an EXPRESSION       i < 3; this is a CONDITION    TOGETHER they are a STATEMENT    I++ is equal to i = i + 1, INCREMENT EXPRESSION
+    //console.log(enemyNames[i]);
+    //console.log(i);
+    //console.log(enemyNames[i] + " is at " + i + " index");
+}
 
-var fight = function() {
+    //fight function statements
 
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    window.alert("Welcome to Robot Gladiators!");// string argument
 
     // Prompts players to choose between fighting or skipping the fight
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -63,4 +74,6 @@ var fight = function() {
             window.alert("You need to choose a valid option. Try again!");
         }
     };    
-    fight();//dhfkjsdhkjfsdhfksdhjkfhsdjkfhkjdshfjkhsdkjfhdjshfkjsdhkjfhsdkjfhkjdshfkjsdhkjfhsdkfhjkdsh
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
